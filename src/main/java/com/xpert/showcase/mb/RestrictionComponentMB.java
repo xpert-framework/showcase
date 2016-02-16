bd346d77882ed52f7416f9e56c75dc5e890f02d6
ayslanms <ayslanms@gmail.com>
2016-02-16 17:17:31 -0300

    restrictionCollector component

--- src/main/java/com/xpert/showcase/mb/RestrictionComponentMB.java
+++ src/main/java/com/xpert/showcase/mb/RestrictionComponentMB.java
@@ -1,16 +1,22 @@
 package com.xpert.showcase.mb;
 
+import com.xpert.faces.component.restriction.RestrictionCollector;
 import com.xpert.faces.primefaces.LazyDataModelImpl;
+import com.xpert.faces.utils.FacesMessageUtils;
 import com.xpert.persistence.query.JoinBuilder;
+import com.xpert.persistence.query.Restriction;
 import com.xpert.persistence.query.Restrictions;
 import com.xpert.showcase.dao.PersonDAO;
 import com.xpert.showcase.model.Person;
 import com.xpert.showcase.model.Status;
+import com.xpert.showcase.vo.PersonVO;
 import java.io.Serializable;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import org.omnifaces.util.Ajax;
 
 /**
  *
@@ -18,29 +24,53 @@ import javax.faces.bean.ViewScoped;
  */
 @ManagedBean
 @ViewScoped
-public class RestrictionComponentMB implements Serializable{
-    
-    
+public class RestrictionComponentMB implements Serializable {
+
     @EJB
     private PersonDAO personDAO;
-    
+
+    private PersonVO personVO = new PersonVO();
+
     private LazyDataModelImpl<Person> dataModelPerson;
     private LazyDataModelImpl<Person> dataModelWithRestriction;
     private LazyDataModelImpl<Person> dataModelPersonWithJoin;
-    
-    
+
+    public void verifyRestrictions() {
+
+        List<Restriction> restrictions = RestrictionCollector.getCurrentRestrictions();
+
+        if (restrictions == null || restrictions.isEmpty()) {
+            FacesMessageUtils.warning("No restriction informed");
+            RestrictionCollector.ignoreRestrictions();
+            return;
+        }
+
+        if (personVO.getBirthdayBegin() != null && personVO.getBirthdayEnd() != null) {
+            if (personVO.getBirthdayBegin().after(personVO.getBirthdayEnd())) {
+                FacesMessageUtils.warning("Birthday begin date is greater then end date");
+                RestrictionCollector.ignoreRestrictions();
+                return;
+            }
+        }
+
+        for (Restriction restriction : restrictions) {
+            FacesMessageUtils.info("Restriction: " + restriction.getProperty() + " " + restriction.getRestrictionType().getSymbol() + " " + restriction.getValue());
+        }
+
+    }
+
     @PostConstruct
-    public void init(){
+    public void init() {
         
         //data model simple
         dataModelPerson = new LazyDataModelImpl("name", personDAO);
-        
+
         //data model with pre defined restriction
         dataModelWithRestriction = new LazyDataModelImpl("name", personDAO);
         Restrictions restrictions = new Restrictions();
         restrictions.add("status", Status.ACTIVE);
         dataModelWithRestriction.setRestrictions(restrictions);
-        
+
         //data model with Join Builder
         dataModelPersonWithJoin = new LazyDataModelImpl("p.name", personDAO);
         JoinBuilder joinBuilder = new JoinBuilder("p");
@@ -71,8 +101,13 @@ public class RestrictionComponentMB implements Serializable{
     public void setDataModelPersonWithJoin(LazyDataModelImpl<Person> dataModelPersonWithJoin) {
         this.dataModelPersonWithJoin = dataModelPersonWithJoin;
     }
-    
-    
-    
-    
+
+    public PersonVO getPersonVO() {
+        return personVO;
+    }
+
+    public void setPersonVO(PersonVO personVO) {
+        this.personVO = personVO;
+    }
+
 }
---
+++ src/main/java/com/xpert/showcase/vo/PersonVO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xpert.showcase.vo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ayslan.sousa
+ */
+public class PersonVO {
+    
+    private Date birthdayBegin;
+    private Date birthdayEnd;
+
+    public Date getBirthdayBegin() {
+        return birthdayBegin;
+    }
+
+    public void setBirthdayBegin(Date birthdayBegin) {
+        this.birthdayBegin = birthdayBegin;
+    }
+
+    public Date getBirthdayEnd() {
+        return birthdayEnd;
+    }
+
+    public void setBirthdayEnd(Date birthdayEnd) {
+        this.birthdayEnd = birthdayEnd;
+    }
+    
+    
+    
+}
